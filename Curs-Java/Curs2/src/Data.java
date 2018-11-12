e3c40abddf0c4b5bb29685dd5f0ba189a08f41dd
alexadumi <alexandra.dumitru1218@gmail.com>
2018-11-13 00:50:12 +0200

    Gobo

--- Curs-Java/Curs2/src/Data.java
+++ Curs-Java/Curs2/src/Data.java
@@ -3,44 +3,57 @@ public class Data {
         int myAge = 29;
         float weight = 60.5f;
         double height = 356.6;
-        long distance = 10231220002331112l;
+        long distance = 102312331;
         char gender = 'm';
+        char test = 9;
         boolean isStudent = false;
         boolean hasJob = true;
-        String mesaj = "Hello";
-        String mesaj2 = "Goodbye";
-        String mesaj3 = mesaj + mesaj2;
-        System.out.println(mesaj3);
-        System.out.println("Hello");
-        double a = 5;
-        int b = 2;
-        double c = a / b;
-        System.out.println(c);
-
-
-        int d = 5;
-        int e = 2;
-        int f = d - e;
-        System.out.println(f);
-
+        int test2 = (int) weight;
+        System.out.println(test);
+        System.out.println(test2);
         int x = 10;
-        int y = x++;
-        System.out.println(x);
-
-        boolean youAreHappy = true;
-        if (youAreHappy) {
+        int y = ++x;
+        System.out.println(y);
+        x = 10;
+        y = x++;
+        System.out.println(y);
+        boolean noMoreCoffe = false;
+        if (noMoreCoffe) {
+            System.out.println("Refill");
+            System.out.println("Drink coffe");
+        }
+        boolean happy = true;
+        if (happy) {
             System.out.println("Clap your hands");
-            System.out.println("Smile");
         } else {
-            System.out.println("Do not worry");
+            System.out.println("Don't worry");
         }
-        int p = 0;
-        while (p < 3) {
-            System.out.println(p);
-            p++;
-
+        boolean snowing = true;
+        boolean raining = true;
+        if (snowing) {
+            System.out.println("Play with snow");
+        }else if (raining) {
+            System.out.println("Take your umbrella");
         }
+         else {
+             System.out.println("Stay home");
+         }
+         int i = 10;
+         int j = 10;
+         if(i==j) {
+             System.out.println("Sunt egale");
+         } else {
+                 System.out.println("Nu sunt egale");
+             }
 
+             int p = 0 ;
+         while (p < 3) {
+             System.out.println(p);
+             p++;
+
+         }
+
+         }
     }
 
 
@@ -49,10 +62,18 @@ public class Data {
 
 
 
-                ;
 
 
 
 
-    }
+
+
+
+
+
+
+
+
+
+
 
